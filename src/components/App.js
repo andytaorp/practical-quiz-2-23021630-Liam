631e6d952e0c1a2a2cf0a7266879add14c0465dd
liam <23021630@myrp.edu.sg>
2024-11-27 16:06:51 +0800

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -6,22 +6,36 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   const handleAddTask = (newTask) => {
-    // TODO: write code to add a new task
+    console.log("Adding task:", newTask);
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: Date.now(), text: newTask, completed: false },
+    ]);
   };
 
   const handleToggleTask = (id) => {
-    // TODO: write code to toggle a task's status
+    console.log("Toggling task with id:", id);
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
   };
 
   const handleDeleteTask = (id) => {
-    // TODO: write code to delete a task
+    console.log("Deleting task with id:", id);
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
 
   return (
     <div className="app">
-      <h1>Task Tracker</h1>
-      {/*TODO: add a form to add a new task*/}
-      {/*TODO: add a list of tasks*/}
+      <h1>Task Tracker</h1> 
+      <Form onAddTask={handleAddTask} />
+      <TaskList
+        tasks={tasks}
+        onToggleTask={handleToggleTask}
+        onDeleteTask={handleDeleteTask}
+      />
     </div>
   );
 }
